f546df25a1c11909e5fa1bb88acb9d79ce23a06b
Ella Zamyatina <ellazamyatina567@gmail.com>
2025-12-18 21:15:16 +0300

    Add noqa for strings that ruff doesnt like

--- hw4/exchange_coins.py
+++ hw4/exchange_coins.py
@@ -6,14 +6,14 @@ def exchange(n):
     dp[0] = 0  # start point for program
     for i in range(1, n + 1):
         for coin in coins:
-            if i >= coin and dp[i - coin] is not None:
+            if i >= coin and dp[i - coin] is not None:  # noqa: SIM102
                 if dp[i] is None or dp[i - coin] + 1 < dp[i]:
                     dp[i] = dp[i - coin] + 1
                     last_sum[i] = i - coin
                     last_coin[i] = coin
     if dp[n] is None:
         return None
-    else:
+    else:  # noqa: RET505
         change = []
         current_sum = n
         while current_sum > 0:
@@ -35,5 +35,5 @@ try:  # use "try except". eg for non-numeric inputs
             print("-42!")
 
 
-except:
+except:  # noqa: E722
     print("-42!")
