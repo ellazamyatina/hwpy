3d613e7c6de626882cea76e3ffadda7346a03188
Ella Zamyatina <ellazamyatina567@gmail.com>
2025-12-18 20:53:29 +0300

    Trigger for CI

--- hw1/rasheuclid.py
+++ hw1/rasheuclid.py
@@ -1,6 +1,7 @@
-def gcd(a,b):
+def gcd(a, b):
     if b == 0:
         return a, 1, 0
     else:
-        d, x, y = gcd(b, a%b)
-        return d, y, x-y*(a//b)
+        d, x, y = gcd(b, a % b)
+
+        return d, y, x - y * (a // b)
